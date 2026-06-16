85ead142eef1ad676aae20bb3283946dc939323f
Sandeepk2907 <sandeepkharwar278@gmail.com>
2026-06-16 22:30:14 +0530

    Create dvwa-config.php for DVWA setup
    Added configuration file for DVWA with database settings.

---
+++ configs/dvwa-config.php
@@ -0,0 +1,23 @@
+
+---
+
+##  configs/dvwa-config.php
+```php
+<?php
+
+# DVWA Configuration File
+# Copy this to /var/www/html/DVWA/config/config.inc.php
+
+$_DVWA = array();
+$_DVWA['db_server']   = '127.0.0.1';
+$_DVWA['db_database'] = 'dvwa';
+$_DVWA['db_user']     = 'dvwa';
+$_DVWA['db_password'] = 'password';
+$_DVWA['db_port']     = '3306';
+
+$_DVWA['default_security_level'] = 'low';
+
+?>
+```
+
+---
